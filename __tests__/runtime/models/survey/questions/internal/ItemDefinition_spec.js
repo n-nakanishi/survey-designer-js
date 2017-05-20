dd5c115176b96fdd0f471d98adddeed686d9b02b
Jiro Iwamoto <jirokun@gmail.com>
2017-05-21 00:41:47 +0900

    ItemDefinition.isVisibleの追加

---
+++ __tests__/runtime/models/survey/questions/internal/ItemDefinition_spec.js
@@ -0,0 +1,202 @@
+/* eslint-env jest */
+import { List, Map } from 'immutable';
+import SurveyDesignerState from '../../../../../../lib/runtime/models/SurveyDesignerState';
+import OutputDefinition from '../../../../../../lib/runtime/models/survey/questions/internal/OutputDefinition';
+import VisibilityConditionDefinition from '../../../../../../lib/runtime/models/survey/questions/internal/VisibilityConditionDefinition';
+import ItemDefinition from '../../../../../../lib/runtime/models/survey/questions/internal/ItemDefinition';
+import allOutputTypeJson from './ItemDefinition_allOutputType.json';
+
+describe('ItemDefinition', () => {
+  describe('isVisible', () => {
+    describe('設問で選択しているOutputDefinitionのoutputTypeがcheckbox', () => {
+      it('選択しているが選ばれている場合', () => {
+        let survey = SurveyDesignerState.createFromJson({ survey: allOutputTypeJson }).getSurvey();
+        const outputDefinitions = survey.getAllOutputDefinitions();
+        const targetOutputDefinition = outputDefinitions.get(0);
+        const visibilityCondition = new VisibilityConditionDefinition({
+          _id: 'vcd1',
+          outputDefinitionId: targetOutputDefinition.getId(),
+          operator: '!!',
+          value: 'true',
+        });
+        survey = survey.updateIn(['pages', 0, 'questions', 1, 'items', 0], item => item.set('visibilityCondition', visibilityCondition));
+        const item = survey.getIn(['pages', 0, 'questions', 1, 'items', 0]);
+        expect(targetOutputDefinition.getOutputType()).toBe('checkbox');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '1' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '0' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(false);
+        expect(item.isVisible(survey, Map())).toBe(false);
+      });
+
+      it('選択していないが選ばれている場合', () => {
+        let survey = SurveyDesignerState.createFromJson({ survey: allOutputTypeJson }).getSurvey();
+        const outputDefinitions = survey.getAllOutputDefinitions();
+        const targetOutputDefinition = outputDefinitions.get(0);
+        const visibilityCondition = new VisibilityConditionDefinition({
+          _id: 'vcd1',
+          outputDefinitionId: targetOutputDefinition.getId(),
+          operator: '!!',
+          value: 'false',
+        });
+        survey = survey.updateIn(['pages', 0, 'questions', 1, 'items', 0], item => item.set('visibilityCondition', visibilityCondition));
+        const item = survey.getIn(['pages', 0, 'questions', 1, 'items', 0]);
+        expect(targetOutputDefinition.getOutputType()).toBe('checkbox');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '1' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '0' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(true);
+        expect(item.isVisible(survey, Map())).toBe(true);
+      });
+    });
+
+    describe('設問で選択しているOutputDefinitionのoutputTypeがradio', () => {
+      it('選択しているが選ばれている場合', () => {
+        let survey = SurveyDesignerState.createFromJson({ survey: allOutputTypeJson }).getSurvey();
+        const outputDefinitions = survey.getAllOutputDefinitions();
+        const targetOutputDefinition = outputDefinitions.get(1);
+        const visibilityCondition = new VisibilityConditionDefinition({
+          _id: 'vcd1',
+          outputDefinitionId: targetOutputDefinition.getId(),
+          operator: '!!',
+          value: 'true',
+        });
+        survey = survey.updateIn(['pages', 0, 'questions', 0, 'items', 0], item => item.set('visibilityCondition', visibilityCondition));
+        const item = survey.getIn(['pages', 0, 'questions', 0, 'items', 0]);
+        expect(targetOutputDefinition.getOutputType()).toBe('radio');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '1' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '0' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(false);
+        expect(item.isVisible(survey, Map())).toBe(false);
+      });
+
+      it('選択していないが選ばれている場合', () => {
+        let survey = SurveyDesignerState.createFromJson({ survey: allOutputTypeJson }).getSurvey();
+        const outputDefinitions = survey.getAllOutputDefinitions();
+        const targetOutputDefinition = outputDefinitions.get(1);
+        const visibilityCondition = new VisibilityConditionDefinition({
+          _id: 'vcd1',
+          outputDefinitionId: targetOutputDefinition.getId(),
+          operator: '!!',
+          value: 'false',
+        });
+        survey = survey.updateIn(['pages', 0, 'questions', 0, 'items', 0], item => item.set('visibilityCondition', visibilityCondition));
+        const item = survey.getIn(['pages', 0, 'questions', 0, 'items', 0]);
+        expect(targetOutputDefinition.getOutputType()).toBe('radio');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '1' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '0' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(true);
+        expect(item.isVisible(survey, Map())).toBe(true);
+      });
+    });
+
+    describe('設問で選択しているOutputDefinitionのoutputTypeがnumber', () => {
+      function getTargetOutputDefinition(survey) {
+        const outputDefinitions = survey.getAllOutputDefinitions();
+        return outputDefinitions.get(2);
+      }
+      function getSurvey(comparisonType, operator, value) {
+        const survey = SurveyDesignerState.createFromJson({ survey: allOutputTypeJson }).getSurvey();
+        const targetOutputDefinition = getTargetOutputDefinition(survey);
+        const visibilityCondition = new VisibilityConditionDefinition({
+          _id: 'vcd1',
+          outputDefinitionId: targetOutputDefinition.getId(),
+          comparisonType,
+          operator,
+          value,
+        });
+        return survey.updateIn(['pages', 0, 'questions', 0, 'items', 0], item => item.set('visibilityCondition', visibilityCondition));
+      }
+      function getItem(survey) {
+        return survey.getIn(['pages', 0, 'questions', 0, 'items', 0]);
+      }
+
+      it('fixedValueと==が選ばれている場合', () => {
+        const survey = getSurvey('fixedValue', '==', '10');
+        const item = getItem(survey);
+        const targetOutputDefinition = getTargetOutputDefinition(survey);
+        expect(targetOutputDefinition.getOutputType()).toBe('number');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '10' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '0' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(false);
+        expect(item.isVisible(survey, Map())).toBe(false);
+      });
+
+      it('fixedValueと!=が選ばれている場合', () => {
+        const survey = getSurvey('fixedValue', '!=', '10');
+        const item = getItem(survey);
+        const targetOutputDefinition = getTargetOutputDefinition(survey);
+        expect(targetOutputDefinition.getOutputType()).toBe('number');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '10' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '0' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(true);
+        expect(item.isVisible(survey, Map())).toBe(true);
+      });
+
+      it('fixedValueと<が選ばれている場合', () => {
+        const survey = getSurvey('fixedValue', '<', '10');
+        const item = getItem(survey);
+        const targetOutputDefinition = getTargetOutputDefinition(survey);
+        expect(targetOutputDefinition.getOutputType()).toBe('number');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '10' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '9' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '9.9' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '0' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(false);
+        expect(item.isVisible(survey, Map())).toBe(false);
+      });
+
+      it('fixedValueと<=が選ばれている場合', () => {
+        const survey = getSurvey('fixedValue', '<=', '10');
+        const item = getItem(survey);
+        const targetOutputDefinition = getTargetOutputDefinition(survey);
+        expect(targetOutputDefinition.getOutputType()).toBe('number');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '11' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '10' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '9.9' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(false);
+        expect(item.isVisible(survey, Map())).toBe(false);
+      });
+
+      it('fixedValueと>が選ばれている場合', () => {
+        const survey = getSurvey('fixedValue', '>', '10');
+        const item = getItem(survey);
+        const targetOutputDefinition = getTargetOutputDefinition(survey);
+        expect(targetOutputDefinition.getOutputType()).toBe('number');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '11' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '10' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(false);
+        expect(item.isVisible(survey, Map())).toBe(false);
+      });
+
+      it('fixedValueと>=が選ばれている場合', () => {
+        const survey = getSurvey('fixedValue', '>=', '10');
+        const item = getItem(survey);
+        const targetOutputDefinition = getTargetOutputDefinition(survey);
+        expect(targetOutputDefinition.getOutputType()).toBe('number');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '11' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '10' }))).toBe(true);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '9.9' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: '' }))).toBe(false);
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: null }))).toBe(false);
+        expect(item.isVisible(survey, Map())).toBe(false);
+      });
+
+      it('answerValueと==が選ばれている場合', () => {
+        const survey = getSurvey('answerValue', '==', '10');
+        const item = getItem(survey);
+        const targetOutputDefinition = getTargetOutputDefinition(survey);
+        expect(targetOutputDefinition.getOutputType()).toBe('number');
+        expect(item.isVisible(survey, Map({ [targetOutputDefinition.getName()]: `{{${targetOutputDefinition.getId()}.answer_value}}` }))).toBe(true);
+      });
+    });
+  });
+});
+
